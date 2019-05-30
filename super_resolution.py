7a59ddcb9030d6a935a23a0a00442d8974733b32
avivga <avivga@gmail.com>
2019-05-30 14:45:46 +0300

    refactoring

--- super_resolution.py
+++ super_resolution.py
@@ -29,36 +29,26 @@ def optimize_latent_codes(args):
 	generated_img = Gs.components.synthesis.get_output_for(latent_code, randomize_noise=False)
 	generated_img = tf.transpose(generated_img, [0, 2, 3, 1])
 	generated_img = ((generated_img + 1) / 2) * 255
+	generated_img = tf.image.resize_images(generated_img, tuple(args.hr_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+	generated_lr_img = tf.image.resize_images(generated_img, tuple(args.lr_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+	generated_img_for_display = tf.saturate_cast(generated_img, tf.uint8)
 
-	target_img = tf.placeholder(tf.float32, [None, args.input_img_size[0], args.input_img_size[1], 3])
-	target_img_resized = tf.image.resize_images(
-		target_img, tuple(args.perceptual_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
-	)
+	lr_img = tf.placeholder(tf.float32, [None, args.lr_img_size[0], args.lr_img_size[1], 3])
 
-	generated_img_resized_to_original = tf.image.resize_images(
-		generated_img, tuple(args.input_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
-	)
-
-	generated_img_resized = tf.image.resize_images(
-		generated_img_resized_to_original, tuple(args.perceptual_img_size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
-	)
-
-	generated_img_for_display = tf.saturate_cast(generated_img_resized_to_original, tf.uint8)
-
-	perceptual_model = PerceptualModel(img_size=(args.input_img_size[0], args.input_img_size[1]))
-	generated_img_features = perceptual_model(generated_img_resized)
-	target_img_features = perceptual_model(target_img_resized)
+	perceptual_model = PerceptualModel(img_size=(args.lr_img_size[0], args.lr_img_size[1]))
+	generated_img_features = perceptual_model(generated_lr_img)
+	target_img_features = perceptual_model(lr_img)
 
 	loss_op = tf.reduce_mean(tf.abs(generated_img_features - target_img_features))
 
-	optimizer = tf.train.GradientDescentOptimizer(learning_rate=args.learning_rate)
+	optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
 	train_op = optimizer.minimize(loss_op, var_list=[latent_code])
 
 	sess = tf.get_default_session()
 
-	img_names = sorted(os.listdir(args.imgs_dir))
+	img_names = sorted(os.listdir(args.lr_imgs_dir))
 	for img_name in img_names:
-		img = imageio.imread(os.path.join(args.imgs_dir, img_name))
+		img = imageio.imread(os.path.join(args.lr_imgs_dir, img_name))
 
 		sess.run(tf.variables_initializer([latent_code] + optimizer.variables()))
 
@@ -72,37 +62,37 @@ def optimize_latent_codes(args):
 			loss, _ = sess.run(
 				fetches=[loss_op, train_op],
 				feed_dict={
-					target_img: img[np.newaxis, ...]
+					lr_img: img[np.newaxis, ...]
 				}
 			)
 
 			progress_bar_iterator.set_postfix_str('loss=%.2f' % loss)
 
-		reconstructed_imgs, latent_codes = sess.run(
+		hr_imgs, latent_codes = sess.run(
 			fetches=[generated_img_for_display, latent_code],
 			feed_dict={
-				target_img: img[np.newaxis, ...]
+				lr_img: img[np.newaxis, ...]
 			}
 		)
 
-		imageio.imwrite(os.path.join(args.reconstructions_dir, img_name), reconstructed_imgs[0])
+		imageio.imwrite(os.path.join(args.hr_imgs_dir, img_name), hr_imgs[0])
 		np.savez(file=os.path.join(args.latents_dir, img_name + '.npz'), latent_code=latent_codes[0])
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--imgs-dir', type=str, required=True)
-	parser.add_argument('--reconstructions-dir', type=str, required=True)
+	parser.add_argument('--lr-imgs-dir', type=str, required=True)
+	parser.add_argument('--hr-imgs-dir', type=str, required=True)
 	parser.add_argument('--latents-dir', type=str, required=True)
 
-	parser.add_argument('--input-img-size', type=int, nargs=2, default=(1024, 1024))
-	parser.add_argument('--perceptual-img-size', type=int, nargs=2, default=(128, 128))
-	parser.add_argument('--learning-rate', type=float, default=1e-2)
+	parser.add_argument('--lr-img-size', type=int, nargs=2, default=(128, 128))
+	parser.add_argument('--hr-img-size', type=int, nargs=2, default=(1024, 1024))
+	parser.add_argument('--learning-rate', type=float, default=1e-3)
 	parser.add_argument('--total-iterations', type=int, default=1000)
 
 	args = parser.parse_args()
 
-	os.makedirs(args.reconstructions_dir, exist_ok=True)
+	os.makedirs(args.hr_imgs_dir, exist_ok=True)
 	os.makedirs(args.latents_dir, exist_ok=True)
 
 	optimize_latent_codes(args)
